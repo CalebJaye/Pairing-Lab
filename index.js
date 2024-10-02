6e1632579d4fe8f8bd882626aee731df8271dc6b
Jordid13 <jordidiaz2020@gmail.com>
2024-10-02 12:59:20 -0400

    Completed task #2

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+//Task #1 (Caleb)
+
 const fiveToOneHundred = () => {
     for (let i = 5; i <= 100; i++){
-console.log(i)
+        console.log(i)
     } 
 }
 fiveToOneHundred()
+
+// Task #2 (Jordi)
+
+const multiplesOfThree = () => {
+    for (let i = 0; i <= 100; i+= 3){
+        console.log(i)
+    } 
+}
+
+multiplesOfThree();
+
+// Task #3 (Caleb)
